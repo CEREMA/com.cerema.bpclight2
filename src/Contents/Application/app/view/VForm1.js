7e3c7a7731ed5b40d3c6fe5358413a8a9d5bb879
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-06-29 21:07:24 +0200

    Update# 45

---
+++ src/Contents/Application/app/view/VForm1.js
@@ -0,0 +1,113 @@
+App.view.define('VForm1', {
+    extend: "Ext.window.Window",
+    alias: 'widget.TForm1',
+    initComponent: function() {
+        this.width = 1024;
+        this.height = 660;
+        this.title = "...";
+
+        this.layout = {
+            type: 'border'
+        };
+
+        this.bbar = [
+            '->', {
+                text: 'Enregistrer',
+				itemId: "Record"
+            }, {
+                text: 'Quitter',
+				itemId: "Exit"
+            }
+        ];
+		
+        this.defaults = {
+            split: true
+        };
+
+        this.items = [
+		{
+            region: 'west',
+            xtype: "panel",
+            width: 200,
+            minWidth: 200,
+            height: "100%",
+            border: false,
+            itemId: "MenuPanel",
+            items: [{
+                xtype: "TSidePanel",
+                border: false
+            }]
+        }, {
+            region: 'center',
+			items: [
+			{
+                border: false,
+                xtype: "tabpanel",
+				itemId: "tabs",
+                items: [{
+                    xtype: "TAgent"
+                }, {
+                    xtype: "TSituation"
+                },
+				{
+                    xtype: "TFormation"
+                },
+				{
+					xtype: "TVisit",
+					itemId: "tab_medic"
+				}]
+            }			
+			]
+		},
+		{
+			region: 'east',
+			width: 250,
+			layout: "vbox",
+			title: "Métier",
+			border: false,
+			items: [
+				{
+                    xtype: "htmleditor",
+					itemId: "metier",
+                    width: "100%",
+                    flex: 1
+                }, {
+                    xtype: "grid",
+					itemId: "roles",
+                    flex: 1,
+                    tbar: [
+						{
+							xtype: "combo",
+							itemId: "cboRoles",
+							editable: false,
+							fieldLabel: "Rôles",
+							labelAlign: "left",
+							labelWidth: 40,
+							width: 200,
+							store: App.store.create("App.Roles.getAll"),
+							displayField: "LibRol",
+							valueField: "Krol",
+							matchFieldWidth: false
+						},
+                        '->', {
+                            iconCls: "plus",
+							itemId: "RoleAdd"
+                        }
+                    ],
+                    columns: [{
+                        text: "Rôle",
+						dataIndex: "LibRol",
+                        flex: 1
+                    }],
+                    store: App.store.create('App.Agents.getRole'),
+					selType: 'cellmodel',	
+                    width: "100%",
+                    flex: 1
+                }
+			]
+		}
+		];
+
+        this.callParent();
+    }
+});
