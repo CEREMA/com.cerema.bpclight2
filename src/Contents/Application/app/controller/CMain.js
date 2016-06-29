a7e6495b3562a3ad999a7979b60087739cfd9b18
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-06-29 19:41:17 +0200

    Update# 32

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -14,7 +14,10 @@ App.controller.define('CMain', {
 		this.control({
 			"menu>menuitem": {
 				click: "Menu_onClick"
-			}
+			},
+			"mainform treepanel#TreePanel": {
+				itemclick: "tree_onclick"
+			}			
 		});
 		
 		App.init('VMain',this.onLoad);
@@ -25,6 +28,21 @@ App.controller.define('CMain', {
 		if (p.itemId) {
 			Ext.Msg.alert('Status', 'Click event on '+p.itemId);
 		};			
+	},
+	tree_onclick: function(p, record, item, index)
+	{
+		var id=record.data.id;
+		var grid=App.get('mainform grid#GridAgents');
+		if (id.indexOf('Ksub')>-1) grid.getStore().getProxy().extraParams={
+			ksub: id.split('Ksub')[1]
+		}; else
+		if (id.indexOf('Kuni')>-1) grid.getStore().getProxy().extraParams={
+			kuni: id.split('Kuni')[1]
+		}; else
+		if (id.indexOf('Kets')>-1) grid.getStore().getProxy().extraParams={
+			kets: id.split('Kets')[1]
+		};
+		grid.getStore().load();	
 	},
 	onLoad: function()
 	{
