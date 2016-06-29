f2e9c2219fb655a8f1cfa2aeba13d45d38630798
Stéphane Zucatti <stephane.zucatti@cerema.fr>
2016-06-29 21:07:30 +0200

    Update# 46

---
+++ src/Contents/Application/app/view/agent/VAutorisation.js
@@ -0,0 +1,12 @@
+App.view.define('agent.VAutorisation', {
+	alias: "widget.TAutorisation",
+	extend: "Ext.Panel",
+	initComponent: function()
+	{
+		this.title="Pièces diverses & Autorisations";
+		this.layout="vbox";
+		this.border=false;
+		this.items = [
+		];
+	}
+});
