f2012b8045b5bd378403fc9c15f3aa16933b7fbc
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-11 15:53:28 +0200

    Update# 170

--- src/Contents/Services/Departements.js
+++ src/Contents/Services/Departements.js
@@ -3,9 +3,9 @@ Departements = {
 	getAll: function(o,cb) {
 		var db=Departements.using('db');
 		if (o.kets) 
-			db.model('bpclight','select * from unites where archive=0 and kets='+o.kets,cb);
+			db.model('bpclight','select * from unites where archive=0 and kets='+o.kets+'  order by LibUniC',cb);
 		else
-			db.model('bpclight','select * from unites where archive=0',cb);
+			db.model('bpclight','select * from unites where archive=0 order by LibUniC',cb);
 	}
 }
 
