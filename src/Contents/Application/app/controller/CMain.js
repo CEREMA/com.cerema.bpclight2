67669ab8d74d33e7564e72c9c8d751f99a91f7bd
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-11 15:49:22 +0200

    Update# 166

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -240,11 +240,10 @@ App.controller.define('CMain', {
 	},	
 	TCAEtablissement_onchange: function(p,record)
 	{
-        console.log(record);
 		App.get(p.up('window'),'combo#TCADepartement').setValue('');
 		App.get(p.up('window'),'combo#TCAService').setValue('');
 		var cbo=App.get(p.up('window'),'combo#TCADepartement');
-		cbo.getStore().getProxy().extraParams.kets=record[0].data.Kets;
+		cbo.getStore().getProxy().extraParams.kets=record.data.Kets;
 		cbo.getStore().load();	
 	},
 	TCADepartement_create: function(_p) 
