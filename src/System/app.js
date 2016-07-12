cfec109df1a3c29df232b72e8bb88c2390327638
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-12 11:20:52 +0200

    Update# 174

--- src/System/app.js
+++ src/System/app.js
@@ -151,6 +151,7 @@ App = {
 							};
 						};			
 						workbook.save(function(ok){
+                            console.log('/tmp/'+uid+'.xlsx');
 							res.end('/tmp/'+uid+'.xlsx');
 						});					
 					});	
