caadb64c6ea6cbc19e829b3caae5340e518a4c2c
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-13 09:46:42 +0200

    Update# 200

--- src/Contents/Application/app/view/agent/VRendezVous.js
+++ src/Contents/Application/app/view/agent/VRendezVous.js
@@ -7,7 +7,10 @@ App.view.define('agent.VRendezVous', {
 		this.layout="vbox";
 		this.border=false;
 		this.items = [
-            
+        {
+            html: "<div class=redtext>L’identification d’un poste à risque doit être validée par le conseiller prévention et le médecin de prévention</div>",
+            padding: 10
+        }
 		];
 		this.callParent();
 	}
