f521f5a465c588f66deb263f97e3531ae90b2966
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-13 10:25:16 +0200

    Update# 212

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -173,7 +173,7 @@ App.controller.define('CAgent', {
 	},
     add_rdv: function()
     {
-        alert('x');
+        App.view.create('TRDVScheduler').show().center();
     },
 	visit_prev: function(p)
 	{
