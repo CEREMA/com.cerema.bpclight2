f1594a5ea1fa8d5c8935810709719cc8f11b8ac1
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-13 09:56:41 +0200

    Update# 202

--- src/Contents/Application/app/view/agent/VRendezVous.js
+++ src/Contents/Application/app/view/agent/VRendezVous.js
@@ -8,8 +8,21 @@ App.view.define('agent.VRendezVous', {
 		this.border=false;
 		this.items = [
         {
-            html: "<div class=redtext>L’identification d’un poste à risque doit être validée par le conseiller prévention et le médecin de prévention</div>",
-            padding: 10
+            html: "<div class=redtext>L’identification d’un poste à risque doit être validée par le conseiller prévention <br>et le médecin de prévention</div>",
+            padding: 10,
+            border: false
+        },
+        {
+            xtype: "checkboxfield",
+            boxLabel: 'Agent occupant un poste à risque'
+        },
+        {
+            xtype: "checkboxfield",
+            boxLabel: 'Dossier demandé'
+        },
+        {
+            xtype: "checkboxfield",
+            boxLabel: 'Dossier reçu'
         }
 		];
 		this.callParent();
