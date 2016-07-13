5b3bae45fa8c9cf044ed88845065076fe5f56ea4
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-13 09:35:14 +0200

    Update# 196

---
+++ src/Contents/Application/app/view/agent/VRendezVous.js
@@ -0,0 +1,14 @@
+App.view.define('agent.VRendezVous', {
+	alias: "widget.TRendezVous",
+	extend: "Ext.Panel",
+	initComponent: function()
+	{
+		this.title="Visites médicales";
+		this.layout="vbox";
+		this.border=false;
+		this.items = [
+            
+		];
+		this.callParent();
+	}
+});
