1897c8a18bf9dcd7aa878f4e48204513210d49f2
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-18 14:34:34 +0200

    Update# 232

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -23,6 +23,13 @@ App.controller.define('CMain', {
 	],
 	
 	models: [
+		"mFonctions",
+		"mGrades",
+		"mBatiments",
+		"mEtablissements",
+		"mDepartements",
+		"mServices",
+		"mCategories"        
 	],
 	
 	init: function()
