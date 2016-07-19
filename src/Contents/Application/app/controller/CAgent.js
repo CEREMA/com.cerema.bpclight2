246add875014a46cc9dd205af04e8b9cc346806a
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-19 11:02:25 +0200

    Update# 234

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -188,6 +188,8 @@ App.controller.define('CAgent', {
     },
     rdv_next: function(me) {
         var scheduler=App.get(me.up('window'),'schedulergrid');
+        var d=new Date();
+        console.log(d.parse);
         console.log(scheduler.startDate);
         //scheduler.setStart(scheduler.startDate.parse('t + 5 d'));
     },
