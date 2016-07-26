466b199532631184e539e5a84c7f13e0fae38cf0
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-26 09:24:44 +0200

    Update# 282

--- src/Contents/Application/app/view/agent/VRendezVous.js
+++ src/Contents/Application/app/view/agent/VRendezVous.js
@@ -62,7 +62,7 @@ App.view.define('agent.VRendezVous', {
                     flex: 1
                 }
             ],
-            store: App.store.create({fields:["nature","dateVisite","resultat","dateFin"],data:[]})
+            store: App.store.create({fields:["nature","dateVisite","resultat","dateFin"],data:[{nature:"yes",dateVisite: "fjsldkfjls"}]})
         }
 		];
 		this.callParent();
