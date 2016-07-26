898c499ab03d623c3393c32fd2f455bba1f364b5
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-26 15:29:40 +0200

    Update# 347

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -196,7 +196,7 @@ App.controller.define('CAgent', {
 		});
 	},
     TRendezVous_onshow: function(me) {
-        var store=App.store.create('bpclight://vm?kage='+me.up('window').agent.Kage);
+        var store=App.store.create('bpclight://medic_rdv?kage='+me.up('window').agent.Kage);
         App.get(me,'grid').bindStore(store);
         App.get(me,'grid').getStore().load();
     },
