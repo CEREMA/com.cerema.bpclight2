42828fe487c47bf15eb200bbfca1f8e4818e71b9
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-27 09:28:27 +0200

    Update# 392

--- src/Contents/Application/app/controller/CMain.js
+++ src/Contents/Application/app/controller/CMain.js
@@ -134,7 +134,7 @@ App.controller.define('CMain', {
 	},
 	rendezVous: function(me)
 	{
-		App.view.create('VRendezVous').show();
+		 App.view.create('VRDVScheduler',{agent:me.up('window').agent,modal: true}).show().center();
 	},
 	NewAgent_onclick: function()
 	{
