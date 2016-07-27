e81b13b59616daae36204a643e33bce6a76fd824
Stéphane Zucatti <stephane.zucatti@me.com>
2016-07-27 09:14:25 +0200

    Update# 384

--- src/Contents/Application/app/controller/CAgent.js
+++ src/Contents/Application/app/controller/CAgent.js
@@ -212,7 +212,7 @@ App.controller.define('CAgent', {
 				text: "Supprimer le rendez-vous",
                 handler: function() {
                     console.log(record.data);
-                    console.log(item);
+                    console.log(index);
                     App.DB.del("bpclight://medic_rdv",[record.data.internalId],function(){
                         me.getStore().load();
                     })
